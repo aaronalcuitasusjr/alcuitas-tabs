cc0977640ad0a7f19d4c5ccb74b91129b7c29ec6
aaronalcuitasusjr <aaronbenjmin.alcuitas.20@usjr.edu.ph>
2022-10-24 23:44:56 +0800

    finished tabs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const url = "https://course-api.com/react-tabs-project";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [index, setIndex] = useState(0);
+
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const newData = await response.json();
+    setData(newData);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="App">
+        <div className="loading-container">Loading...</div>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title-container">Experience</div>
+      <div className="underline"></div>
+      <div className="main-container">
+        <div className="tab-container">
+          {data.map((person, i) => {
+            return (
+              <button
+                key={person.id}
+                className={`tab-item ${i === index && "active"}`}
+                onClick={() => setIndex(i)}
+              >
+                {person.company}
+              </button>
+            );
+          })}
+        </div>
+        <div className="content-container">
+          <p className="job-text">{data[index].title}</p>
+          <p className="name-text">{data[index].company}</p>
+
+          <p className="duration-text">{data[index].dates}</p>
+
+          {data[index].duties.map((duty, index) => {
+            return (
+              <div key={index} className="info-container">
+                <p className="arrows">{"❯❯"}</p>
+                <p className="info-text">{duty}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <button className="more-info-btn">MORE INFO</button>
     </div>
   );
 }
